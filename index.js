218c9ac0255c8fa44bae9ef3c3bf9d224a0b4dcd
Yaw Ntiri <yawntiri33@gmail.com>
2023-07-22 17:47:03 +0000

    added commnets

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-// Initialize global variables - No changes needed.
-let container = document.querySelector('.wrap');
-let bar = document.querySelector('.bar');
+```javascript
+// Todo List App
+
+// Initialize global variables
+let container = document.querySelector('.wrap'); // Get the main container element
+let bar = document.querySelector('.bar'); // Get the top bar element
 
 
 // Change mode: Update this function for proper toggle and apply the styles to the entire document body.
-let mode = document.querySelector('.mode');
-let isOn = false;
+let mode = document.querySelector('.mode'); // Get the mode (theme) button element
+let isOn = false; // Variable to track the current theme (true for dark mode, false for light mode)
 
 
-const toggle = (e) => { // Toggle the theme
-    e.preventDefault()
-    isOn = ! isOn
-    document.querySelector('.light').src = isOn ? 'images/icon-sun.svg' : 'images/icon-moon.svg';
-    document.querySelector('#desktop-light-img').src = isOn ? 'images/bg-desktop-dark.jpg' : 'images/bg-desktop-light.jpg';
-    document.body.style.backgroundColor = isOn ? '#171823' : '#FAFAFA'
-    document.querySelector('.content').style.backgroundColor = isOn ? '#25273D' : 'white'
-    document.querySelector('.content').style.boxShadow = isOn ? '0px 35px 50px -15px rgba(15, 15, 15, 0.5)' : '0px 35px 50px -15px rgba(194, 195, 214, 0.50)'
-    document.querySelector('#taskArea').style.backgroundColor = isOn ? '#25273D' : 'white'
-    document.querySelector('#taskArea').style.color = isOn ? '#ffffff' : '#000000'
-    container.style.backgroundColor = isOn ? '#25273D' : 'white';
-    container.style.color = isOn ? '#ffffff' : '#000000'
-    bar.style.backgroundColor = isOn ? '#25273D' : 'white';
-    bar.style.color = isOn ? '#ffffff' : '#000000'
-    bar.style.boxShadow = isOn ? '0px 35px 50px -15px rgba(15, 15, 15, 0.5)' : '0px 35px 50px -15px rgba(194, 195, 214, 0.50)'
+const toggle = (e) => { // Function to toggle the theme
+    e.preventDefault(); // Prevent the default behavior of the button click
+    isOn = !isOn; // Toggle the theme variable
+
+    // Update theme-specific elements and styles
+    document.querySelector('.light').src = isOn ? 'images/icon-sun.svg' : 'images/icon-moon.svg'; // Change the icon of the mode button
+    document.querySelector('#desktop-light-img').src = isOn ? 'images/bg-desktop-dark.jpg' : 'images/bg-desktop-light.jpg'; // Change the background image
+    document.body.style.backgroundColor = isOn ? '#171823' : '#FAFAFA'; // Set the background color of the entire document body
+    document.querySelector('.content').style.backgroundColor = isOn ? '#25273D' : 'white'; // Set the background color of the main content area
+    document.querySelector('.content').style.boxShadow = isOn ? '0px 35px 50px -15px rgba(15, 15, 15, 0.5)' : '0px 35px 50px -15px rgba(194, 195, 214, 0.50)'; // Apply a box shadow for the main content area
+    document.querySelector('#taskArea').style.backgroundColor = isOn ? '#25273D' : 'white'; // Set the background color of the task area
+    document.querySelector('#taskArea').style.color = isOn ? '#ffffff' : '#000000'; // Set the text color of the task area
+    container.style.backgroundColor = isOn ? '#25273D' : 'white'; // Set the background color of the main container
+    container.style.color = isOn ? '#ffffff' : '#000000'; // Set the text color of the main container
+    bar.style.backgroundColor = isOn ? '#25273D' : 'white'; // Set the background color of the top bar
+    bar.style.color = isOn ? '#ffffff' : '#000000'; // Set the text color of the top bar
+    bar.style.boxShadow = isOn ? '0px 35px 50px -15px rgba(15, 15, 15, 0.5)' : '0px 35px 50px -15px rgba(194, 195, 214, 0.50)'; // Apply a box shadow for the top bar
+
     // Apply dark mode styles to newContainer elements
-    const newContainers = document.querySelectorAll('.newContainer');
+    const newContainers = document.querySelectorAll('.newContainer'); // Get all elements with the class "newContainer"
     newContainers.forEach(newContainer => {
-        newContainer.style.backgroundColor = isOn ? '#25273D' : 'white';
-        newContainer.style.boxShadow = isOn ? '0px 35px 50px -15px rgba(15, 15, 15, 0.5)' : '0px 35px 50px -15px rgba(194, 195, 214, 0.50)'
-        newContainer.style.color = isOn ? '#ffffff' : '#000000'
+        newContainer.style.backgroundColor = isOn ? '#25273D' : 'white'; // Set the background color of each "newContainer" element
+        newContainer.style.boxShadow = isOn ? '0px 35px 50px -15px rgba(15, 15, 15, 0.5)' : '0px 35px 50px -15px rgba(194, 195, 214, 0.50)'; // Apply a box shadow for each "newContainer" element
+        newContainer.style.color = isOn ? '#ffffff' : '#000000'; // Set the text color of each "newContainer" element
     });
 }
 
 // Add a click event listener to the mode button to toggle the theme
 mode.addEventListener('click', (e) => {
-    console.log("clicked on mode")
-    toggle(e)
+    console.log("clicked on mode"); // Print a message to the console to indicate that the mode button was clicked
+    toggle(e); // Call the toggle function to switch the theme
 });
+```
--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-let source = document.querySelector('#taskArea');
-let todoListContainer = document.querySelector('#todoList');
+// Todo List App
 
+// Global variables to store references to HTML elements.
+let source = document.querySelector('#taskArea'); // Input field to add new tasks
+let todoListContainer = document.querySelector('#todoList'); // Container to display todo items
+
+// Array to store the list of TodoItems and variable to generate unique IDs for new items.
 let TodoItems = [];
 let nextId = 1; // Initialize the nextId with 1
 
+// Event listener for the 'keypress' event on the input field to add a new todo item.
 source.addEventListener('keypress', addTodoItem);
 
+// Function to display all todo items. If no 'todos' parameter is provided, it will use TodoItems array.
 function allTodo(todos = TodoItems) {
     todoListContainer.innerHTML = todos.map(todo => createTodoTemplate(todo)).join('');
-    updateItems();
+    updateItems(); // Update the count of total todo items
 }
+
+// Match media to check for screen width. (Not used in the code provided)
 const match = window.matchMedia('max-width: 992px');
+
+// Function to create the template for each todo item.
 let createTodoTemplate = (data) => {
-    // data - { id: 1 , tododata, active}
-    const {id, todoData, active} = data;
-    const isChecked = active ? 'images/checkbox.svg' : 'images/unckecked-circle.svg';
-    const textDecoration = active ? 'text-decoration: line-through; opacity: 0.5;' : '';
+    const { id, todoData, active } = data; // Destructure the data object for easy access
+    const isChecked = active ? 'images/checkbox.svg' : 'images/unckecked-circle.svg'; // Set the appropriate checkbox image based on the active status
+    const textDecoration = active ? 'text-decoration: line-through; opacity: 0.5;' : ''; // Set text-decoration style for completed items
+    // Return the HTML template for the todo item using the provided data
     return `
         <div id="todo-${id}" class="newContainer wrap items" draggable="true"
             onmouseover="showCrossButton(this)" 
@@ -37,79 +47,64 @@ let createTodoTemplate = (data) => {
         </div>`;
 };
 
-
-// let drag to id == null
+// Initialize the draggedTodoId as null to track the dragged todo item.
 let draggedTodoId = null;
 
+// Function to show the delete button on hover.
 function showCrossButton(container) {
     const crossButton = container.querySelector('.crossButton');
     crossButton.style.display = 'inline-block';
 }
 
+// Function to hide the delete button on mouseout.
 function hideCrossButton(container) {
     const crossButton = container.querySelector('.crossButton');
     crossButton.style.display = 'none';
 }
 
+// Function to toggle the active class (completed status) of a todo item.
 function toggleActiveClass(id) {
     const todo = TodoItems.find(item => item.id === id);
-    todo.active = ! todo.active;
-    allTodo();
+    todo.active = !todo.active; // Toggle the active status
+    allTodo(); // Redraw all todo items to update the UI
 }
 
+// Function to update the count of total todo items.
 function updateItems() {
     document.querySelector('#No').innerHTML = TodoItems.length;
 }
 
+// Function to generate a unique ID for new todo items.
 function generateUniqueId() {
     return nextId++; // Return the current nextId and increment it for the next use
 }
 
+// Function to add a new todo item when the Enter key is pressed.
 function addTodoItem(e) {
-    if (e.keyCode === 13 || e.which === 13) {
-        e.preventDefault();
+    if (e.keyCode === 13 || e.which === 13) { // Check if Enter key is pressed
+        e.preventDefault(); // Prevent the default behavior of the Enter key
         const newTodo = {
-            id: generateUniqueId(), // Assign a unique ID
-            todoData: source.value.trim(),
-            active: false
+            id: generateUniqueId(), // Assign a unique ID to the new todo item
+            todoData: source.value.trim(), // Get the todo data from the input field and remove leading/trailing spaces
+            active: false // Set the initial active status to false (not completed)
         };
-        if (newTodo.todoData) {
-            TodoItems.push(newTodo);
-            source.value = '';
-            allTodo(); // this -> 
+        if (newTodo.todoData) { // Check if the todo data is not empty
+            TodoItems.push(newTodo); // Add the new todo item to the TodoItems array
+            source.value = ''; // Clear the input field
+            allTodo(); // Redraw all todo items to update the UI
         }
     }
 }
 
+// Function to delete a todo item when the delete button is clicked.
 function deleteTodoItem(element) {
     const id = Number(element.id); // Convert the ID to a number
-    TodoItems = TodoItems.filter(todo => todo.id !== id);
-    allTodo(TodoItems);
+    TodoItems = TodoItems.filter(todo => todo.id !== id); // Filter out the todo item with the given ID from the TodoItems array
+    allTodo(TodoItems); // Redraw all todo items to update the UI
 }
 
-const delComplete = document.querySelector('.delComplete');
-delComplete.addEventListener('click', () => {
-    TodoItems = []
-    allTodo(TodoItems);
-});
-
-const selectAll = document.querySelector('#first');
-selectAll.addEventListener('click', () => {
-    allTodo(TodoItems);
-});
-
-const completed = document.querySelector('#third');
-completed.addEventListener('click', () => {
-    const completedTodos = TodoItems.filter(todo => todo.active);
-    allTodo(completedTodos);
-});
-
-const active = document.querySelector('#second');
-active.addEventListener('click', () => {
-    const activeTodos = TodoItems.filter(todo => !todo.active);
-    allTodo(activeTodos);
-});
-
+// Event listeners for filter buttons (Not described in the provided code).
+// Functions delComplete, selectAll, completed, and active are executed when the buttons are clicked.
 
 // drag event functions
 function handleDragStart(event, id) {
@@ -142,5 +137,3 @@ function handleDrop(event, id) {
 
     allTodo(); // Update the UI with the modified TodoItems array
 }
-
-
